a216d7ef46979eea21d2262a8df6ea54543f9171
harveytriana <harveytriana@hotmail.com>
2023-09-04 11:50:52 -0500

    Comments

--- LaTeXForms/MainForm.cs
+++ LaTeXForms/MainForm.cs
@@ -1,10 +1,13 @@
-using CSharpMath.Rendering.FrontEnd;
+
 using CSharpMath.SkiaSharp;
 using SkiaSharp;
 
 namespace LaTeXForms;
 
-// rem https://github.com/verybadcat/CSharpMath/issues/226
+// Sample for CSharpMath
+// https://github.com/verybadcat/CSharpMath
+// report document 
+// https://github.com/verybadcat/CSharpMath/issues/226
 
 public partial class MainForm : Form
 {
@@ -19,17 +22,18 @@ public partial class MainForm : Form
     {
         var p = pictureBox; // exists a PictureBox with name pictureBox
 
+        // create canvas
         var imageInfo = new SKImageInfo(p.Width, p.Height);
         using var surface = SKSurface.Create(imageInfo);
-
         using var canvas = surface.Canvas;
 
+        // draw LaTeX 
         var painter = new MathPainter {
             LaTeX = @"\frac\sqrt23"
         };
-
         painter.Draw(canvas);
 
+        // transfer skia image to PictureBox
         using var snapshot = surface.Snapshot();
         using var image = snapshot.Encode(SKEncodedImageFormat.Png, 100);
         using var stream = new MemoryStream(image.ToArray());
